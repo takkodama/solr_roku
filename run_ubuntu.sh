4394b570d8285ad3fd6fd626628bc284186d991c
takschatzl <s1420642@u.tsukuba.ac.jp>
2017-06-25 15:31:50 +0900

    update run_ubuntu.sh

--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -1,14 +1,13 @@
 #/bin/bash
 
 # initiate core (only needed to run first times)
-sudo su - solr -c "$SOLR_HOME/bin/solr delete -c rokudb"
-sudo su - solr -c "$SOLR_HOME/bin/solr delete -c rokureply"
-sudo su - solr -c "$SOLR_HOME/bin/solr restart"
-sudo su - solr -c "$SOLR_HOME/bin/solr create -c rokureply"
+# sudo su - solr -c "$SOLR_HOME/bin/solr delete -c rokureply"
+# sudo su - solr -c "$SOLR_HOME/bin/solr restart"
+# sudo su - solr -c "$SOLR_HOME/bin/solr create -c rokureply"
 
 # post datas to the core
-sudo su - solr -c "$SOLR_HOME/bin/post -c rokureply /home/tk/solr_roku/xml/rokureply.xml"
-sudo su - solr -c "$SOLR_HOME/bin/post -c rokureply /home/tk/solr_roku/json/rokureply.json"
+sudo su - solr -c "$SOLR_HOME/bin/post -c rokureply /home/tk/solr_roku/xml/reply.xml"
+sudo su - solr -c "$SOLR_HOME/bin/post -c rokureply /home/tk/solr_roku/json/reply.json"
 
 # get with http request
 curl "http://localhost:8983/solr/rokureply/select?indent=on&q=id:1&wt=json"
