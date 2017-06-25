316e90e6ba304f8f102950f42839be983ce5ea11
takschatzl <s1420642@u.tsukuba.ac.jp>
2017-06-25 15:52:43 +0900

    add parse example on script/test.py using Japanese language

--- script/test.py
+++ script/test.py
@@ -1,7 +1,10 @@
 import urllib.request
 import simplejson
 
-f = urllib.request.urlopen("http://localhost:8983/solr/rokureply/select?indent=on&q=id:1&wt=json")
+args = "おはよう"
+trigger = urllib.parse.quote_plus(args, encoding='utf-8')
+query = "http://localhost:8983/solr/rokureply/select?indent=on&q=trigger:"+ trigger +"&wt=json"
+f = urllib.request.urlopen(query)
 response = simplejson.load(f)
 print(response['response']['numFound'], "documents found.")
 
