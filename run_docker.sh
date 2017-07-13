a5d9bd44df45e174a838608ca1fe991c9caef185
takschatzl <kodama.takumi.wp@alumni.tsukuba.ac.jp>
2017-07-13 19:47:25 +0900

    remove env variables from run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -6,16 +6,16 @@ declare -a arr=("rokutrigger" "rokureply")
 for i in "${arr[@]}"
 do
    # delete core
-   su - solr -c "$SOLR_HOME/bin/solr delete -c $i"
+   su solr -c "/opt/solr/bin/solr delete -c $i"
 done
 
-su - solr -c "$SOLR_HOME/bin/solr restart"
+su solr -c "/opt/solr/bin/solr restart"
 
 for i in "${arr[@]}"
 do
    # create core
-   su - solr -c "$SOLR_HOME/bin/solr create -c $i"
+   su solr -c "/opt/solr/bin/solr create -c $i"
    # post datas to the core
-   su - solr -c "$SOLR_HOME/bin/post -c $i /solr_roku/json/$i.json"
-   su - solr -c "$SOLR_HOME/bin/post -c $i /solr_roku/xml/$i.xml"
+   su solr -c "/opt/solr/bin/post -c $i /solr_roku/json/$i.json"
+   su solr -c "/opt/solr/bin/post -c $i /solr_roku/xml/$i.xml"
 done
