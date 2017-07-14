2d7a02ab7d22161753f57e1b633d7bdeee271a6c
takschatzl <kodama.takumi.wp@alumni.tsukuba.ac.jp>
2017-07-14 09:36:40 +0900

    add new line to create core

--- run_docker.sh
+++ run_docker.sh
@@ -3,11 +3,11 @@
 # initiate core (only needed to run first times)
 declare -a arr=("rokutrigger" "rokureply")
 
-for i in "${arr[@]}"
-do
-   # delete core
-   solr delete -c $i
-done
+# for i in "${arr[@]}"
+# do
+#    # delete core
+#    solr delete -c $i
+# done
 
 # disable restart so that the app doesn't fail (because of port shutdown?)
 # su solr -c "/opt/solr/bin/solr restart"
@@ -16,7 +16,16 @@ for i in "${arr[@]}"
 do
    # create core
    solr create -c $i
+done
+
+# again
+
+for i in "${arr[@]}"
+do
+   # create core
+   solr create -c $i
+
    # post datas to the core
-   /opt/solr/bin/post -c $i /solr_roku/json/$i.json
-   /opt/solr/bin/post -c $i /solr_roku/xml/$i.xml
+   post -c $i /solr_roku/json/$i.json
+   post -c $i /solr_roku/xml/$i.xml
 done
