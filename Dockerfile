3a22da17201f7a14193f38904962ba2df632a7d4
takschatzl <kodama.takumi.wp@alumni.tsukuba.ac.jp>
2017-07-16 13:11:46 +0900

    add command to input both /xml and /json folder to docker container

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apt-get install -y procps
 RUN rm -rf /usr/solr/solr_roku || true && \
     mkdir -p /usr/solr/solr_roku
 
-ADD ./xml/rokureply.xml /usr/solr/solr_roku/xml/rokureply.xml
-ADD ./json/rokureply.json /usr/solr/solr_roku/json/rokureply.json
+ADD ./xml /usr/solr/solr_roku/xml
+ADD ./json /usr/solr/solr_roku/json
 
 USER solr
--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bash
 
 # build & run the docker container
 docker build -t solr:rokuchanbot .
 docker run -d -p 8983:8983 --name solr_roku solr:rokuchanbot
+docker exec -it --user=solr solr_roku solr start
 
 # initiate core (only needed to run first times)
 declare -a arr=("rokutrigger" "rokureply")
@@ -10,17 +11,17 @@ declare -a arr=("rokutrigger" "rokureply")
 for i in "${arr[@]}"
 do
    # delete core
-   docker exec -it --user=solr solr_roku bin/solr delete -c $i
+   docker exec -it --user=solr solr_roku solr delete -c $i
 done
 
 # restart
-# docker exec -it --user=solr solr_roku bin/solr restart
+# docker exec -it --user=solr solr_roku solr restart
 
 for i in "${arr[@]}"
 do
    # create cores
-   docker exec -it --user=solr solr_roku bin/solr create -c $i
+   docker exec -it --user=solr solr_roku solr create -c $i
    # post datas to the core
-   docker exec -it --user=solr solr_roku bin/post -c "$i" /usr/solr/solr_roku/xml/"$i".xml
-   docker exec -it --user=solr solr_roku bin/post -c "$i" /usr/solr/solr_roku/json/"$i".json
+   docker exec -it --user=solr solr_roku post -c "$i" /usr/solr/solr_roku/xml/"$i".xml
+   docker exec -it --user=solr solr_roku post -c "$i" /usr/solr/solr_roku/json/"$i".json
 done
